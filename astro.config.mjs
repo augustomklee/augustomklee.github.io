2407fbcdefced7f59b80cea06475c719bf1a4003
gutoleeofficial <augusto.leee@gmail.com>
2024-11-19 11:37:26 -0600

    changed domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://augustomklee.github.io",
-  base: "/VURFC-Website",
+  base: "/augustomklee.github.io/",
 });
