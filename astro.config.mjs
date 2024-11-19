b3ec955e26eae8c756cd24999347a1ae792a0c59
gutoleeofficial <augusto.leee@gmail.com>
2024-11-19 11:43:44 -0600

    deleted base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://augustomklee.github.io",
-  base: "/augustomklee.github.io/",
 });
